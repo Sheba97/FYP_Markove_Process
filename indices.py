f6325c4de902c12b0c72e566d1b082c6a43899fb
Sheba97 <lahirushehan164@gmail.com>
2022-05-06 12:34:09 +0530

    Add a equation to find mean down time to indices.py file

--- indices.py
+++ indices.py
@@ -3,7 +3,8 @@ def getIndices(probabilityLst,failurerRate,repaireRate,numberOfSections):
     p_zero = probabilityLst[0]
     p_last = probabilityLst[-1]
     lambdaEq = []
-    uEq = []
+    mdtEq = []
+    numerator = 0
 
     frequencyOfFailure = p_zero * totalFailureRate 
     combinations = list(failurerRate.keys())
@@ -11,16 +12,15 @@ def getIndices(probabilityLst,failurerRate,repaireRate,numberOfSections):
 
     iterations = (pow(2,numberOfSections)-1)
 
-    print(iterations)
 
+    #to create probability dictionary from combination 1 to combination (n-1) 
     for i in range(iterations-1):
         temp = probabilityLst[1:-1]
         a = 2*i
         b = 2*(i+1)
         probabilityDict[combinations[i]] = temp[a:b]
 
-    print(probabilityDict)
-
+    #to find probabilityOfSuccess, frequencyOfSuccess
     for section in combinations[:numberOfSections]:
         probabilityOfSuccess = p_zero
         frequencyOfSuccess = p_last* (repaireRate[combinations[-1]])
@@ -38,9 +38,16 @@ def getIndices(probabilityLst,failurerRate,repaireRate,numberOfSections):
                 else:
                     frequencyOfSuccess += (probabilityDict[state][1]) * repaireRate[state]
         lambdaEq.append(frequencyOfFailure/probabilityOfSuccess)
-        uEq.append(frequencyOfSuccess/(1-probabilityOfSuccess))
-   
-    return lambdaEq,uEq
+        mdtEq.append((1-probabilityOfSuccess)/frequencyOfSuccess)
+    
+    for k in range(numberOfSections):
+        numerator += lambdaEq[k]*mdtEq[k]
+
+    denominator = sum(lambdaEq)
+
+    MDT = numerator/denominator
+
+    return lambdaEq,MDT
 
         
     
